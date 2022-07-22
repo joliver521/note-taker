f49dea7a06d9e6bec79972bfabddfbafb3dce76e
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-07-22 19:14:07 -0400

    added notes to routes files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 
+// function for api requests
 module.exports = app => {
+    // API GET Requests
     app.get('/api/notes', (req, res) => {
         let data = JSON.parse(
             fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8')
@@ -10,16 +12,19 @@ module.exports = app => {
         res.json(data);
     });
 
+    // API POST Requests
     app.post('/api/notes', (req, res) => {
         const newNotes = req.body;
 
         newNotes.id = uuidv4();
 
+        // read the db.json file
         let data = JSON.parse(
             fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8')
         );
         data.push(newNotes);
 
+        // write the new data to the db.json file
         fs.writeFileSync(
             path.join(__dirname, '../db/db.json'),
             JSON.stringify(data)
@@ -27,16 +32,20 @@ module.exports = app => {
         res.json(data);
     });
 
+    // API DELETE Requests
     app.delete('/api/notes/:id', (req, res) => {
+        // find the index of the note to be deleted
         let noteId = req.params.id;
 
         console.log('getting note ID');
 
+        // read the db.json file and parse it into an array
         let data = JSON.parse(
             fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8')
         );
         const newData = data.filter(note => note.id !== noteId);
 
+        // write the new data to the db.json file
         fs.writeFileSync(
             path.join(__dirname, '../db/db.json'),
             JSON.stringify(newData)
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = app => {
+    // HTML GET Requests notes.html
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
+    // HTML GET Requests index.html
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
