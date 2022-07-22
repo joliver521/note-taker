962f11decd74ebcedf52003a0a8a4d4ca50f8a6f
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-07-22 17:21:53 -0400

    set up apiRoutes.js file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+
+module.exports = app => {
+    app.get('/api/notes', (req, res) => {
+        let data = JSON.parse(
+            fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8')
+        );
+        res.json(data);
+    });
+
+    app.post('/api/notes', (req, res) => {
+        const newNotes = req.body;
+
+        newNotes.id = uuidv4();
+
+        let data = JSON.parse(
+            fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8')
+        );
+        data.push(newNotes);
+
+        fs.writeFileSync(
+            path.join(__dirname, '../db/db.json'),
+            JSON.stringify(data)
+        );
+        res.json(data);
+    });
+};
